2fde5297f1c3cb4d1578b7ce4680a8dfd1f96749
hide18 <hide_garmat.1123@suou.waseda.jp>
2022-09-20 11:40:24 +0900

    change

--- train_val.py
+++ train_val.py
@@ -144,7 +144,7 @@ if __name__ == '__main__':
 
 
     transformations = transforms.Compose([
-        transforms.Resize(224),
+        transforms.Resize((448, 448)),
         transforms.ToTensor(),
         transforms.Normalize(
             mean=[0.485, 0.456, 0.406],
